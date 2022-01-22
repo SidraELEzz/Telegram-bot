954d9d51f0aab8cb5b501c7a7d731e64dc35a196
aliyahea <98220565+aliyahea@users.noreply.github.com>
2022-01-22 17:15:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "1816198459:AAEG2c-RD1cTZCJqCW9YcTUf13oYIRJ8vz8"
-APP_URL = "https://sidrabot.herokuapp.com/" + BOT_TOKEN
+BOT_TOKEN = "5200422326:AAHEVa3q16eXE0Yx3JTW5IeBZsbkaYqh8rs"
+APP_URL = "https://github.com/SidraELEzz/Telegram-bot/blob/07a2fbc7f51cad0eca4d12babfdeb07bd0bd72b2/main.py" + BOT_TOKEN
